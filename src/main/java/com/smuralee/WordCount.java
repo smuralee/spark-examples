e7c8c53bf25acc99e8a8f2a61b7db62883edb852
smuralee <suraj@surajmuraleedharan.com>
2020-07-11 22:38:47 +0100

    Updated to write to an out file

--- src/main/java/com/smuralee/WordCount.java
+++ src/main/java/com/smuralee/WordCount.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Tuple2;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -15,6 +17,9 @@ import java.util.regex.Pattern;
 public class WordCount {
     private static final Pattern SPACE = Pattern.compile(" ");
     private static final Logger log = LoggerFactory.getLogger(WordCount.class);
+    private static final String filePrefix = "wordCounts";
+    private static final String newLine = "\n";
+
 
     public static void main(String[] args) throws Exception {
         if (args.length < 1) {
@@ -31,9 +36,21 @@ public class WordCount {
         JavaPairRDD<String, Integer> wordAsTuple = words.mapToPair(word -> new Tuple2<>(word, 1));
         JavaPairRDD<String, Integer> wordWithCount = wordAsTuple.reduceByKey(Integer::sum);
         List<Tuple2<String, Integer>> output = wordWithCount.collect();
+
+        // Create a local file for writing content
+        StringBuilder sb = new StringBuilder(filePrefix);
+        sb.append(System.currentTimeMillis());
+        sb.append(".out.log");
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(sb.toString(), true));
+
         for (Tuple2<?, ?> tuple : output) {
             log.info(tuple._1() + ": " + tuple._2());
+            writer.append(String.valueOf(tuple._1())).append(": ").append(String.valueOf(tuple._2()));
+            writer.append(newLine);
         }
+
+        writer.close();
         ctx.stop();
     }
 }
